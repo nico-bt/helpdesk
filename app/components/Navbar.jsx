17733bf6187a27bb4387e4adda49cba399856c47
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-08-26 19:27:57 -0300

    Add active class for nav and new ticket link

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,16 +1,41 @@
+"use client"
 import Link from "next/link"
 import Image from "next/image"
 import Logo from "./okFace.avif"
+import { usePathname } from "next/navigation"
 
 export default function Navbar() {
+  const path = usePathname()
+
   return (
     <nav>
       <div>
         <Image src={Logo} alt="Helpdesk logo" width={100} placeholder="blur" quality={100} />
         <h1>Helpdesk</h1>
       </div>
-      <Link href="/">Dashboard</Link>
-      <Link href="/tickets">Tickets</Link>
+
+      <Link
+        href="/"
+        className={path == "/" ? "text-black font-bold underline underline-offset-2" : ""}
+      >
+        Dashboard
+      </Link>
+
+      <Link
+        href="/tickets"
+        className={path == "/tickets" ? "text-black font-bold underline underline-offset-2" : ""}
+      >
+        Tickets
+      </Link>
+
+      <Link
+        href="/tickets/create"
+        className={
+          path == "/tickets/create" ? "text-black font-bold underline underline-offset-2" : ""
+        }
+      >
+        New Ticket
+      </Link>
     </nav>
   )
 }
